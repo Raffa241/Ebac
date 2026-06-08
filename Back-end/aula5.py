583e0e557bc2c8676de4bf5f63f08f2644dafac0
Rafael <rafaelsanttos46@gmail.com>
2026-06-08 07:20:05 -0300

    Aula 5

---
+++ Back-end/aula5.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+
+# 1. PIP
+# 2. Poetry
+# 3. Poetry-plugin-shell
+# 4. Comandos "poetry init" e "poetry shell"
---
+++ Back-end/aula8.py
@@ -0,0 +1,47 @@
+#Soma
+
+a = 5
+b = 10
+c = a + b
+
+print(c)
+
+#Subtração
+
+d = 10
+e = 5
+f = d - e
+
+print(f)
+
+#Divisão
+
+g = 10
+h = 5
+i = g / h
+
+print(i)
+
+#Divisão inteira
+
+j = 10
+k = 5
+l = j // k
+
+print(l)
+
+#Multiplicação
+
+m = 10
+n = 5
+o = m * n
+
+print(o)
+
+#Potencialização
+
+p = 2
+q = 3
+r = p ** q
+
+print(r)
